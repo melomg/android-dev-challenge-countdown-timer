eab0ded0f495d76d15562f971a88433b972ab168
Melih Gultekin <mmelihgultekin@gmail.com>
2021-03-06 22:20:30 +0100

    Use typography for texts

--- app/src/main/java/com/example/androiddevchallenge/CountdownTimerView.kt
+++ app/src/main/java/com/example/androiddevchallenge/CountdownTimerView.kt
@@ -20,11 +20,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
@@ -37,13 +35,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.mapper.toCountTimeHolder
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.typography
 import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
@@ -74,45 +71,30 @@ fun CountdownTimerView(
                     .weight(1f)
                     .fillMaxWidth()
             ) {
-                val textSize = 45.sp
                 Text(
                     text = hours,
-                    fontSize = textSize,
-                    fontWeight = FontWeight.Bold,
+                    style = typography.h3,
                     textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .defaultMinSize(40.dp),
                 )
                 Text(
                     text = timerSeparator,
-                    fontSize = textSize,
-                    fontWeight = FontWeight.Bold,
+                    style = typography.h3,
                     textAlign = TextAlign.Center,
                 )
                 Text(
                     text = minutes,
-                    fontSize = textSize,
-                    fontWeight = FontWeight.Bold,
+                    style = typography.h3,
                     textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .defaultMinSize(40.dp),
                 )
                 Text(
                     text = timerSeparator,
-                    fontSize = textSize,
-                    fontWeight = FontWeight.Bold,
+                    style = typography.h3,
                     textAlign = TextAlign.Center,
                 )
                 Text(
                     text = seconds,
-                    fontSize = textSize,
-                    fontWeight = FontWeight.Bold,
+                    style = typography.h3,
                     textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .defaultMinSize(40.dp),
                 )
             }
 
