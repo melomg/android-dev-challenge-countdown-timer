3a47fe5778637020701f10e96f2c8083aaa4c081
Melih Gultekin <mmelihgultekin@gmail.com>
2021-03-07 15:18:05 +0100

    Move timer mapper to timer package

--- app/src/main/java/com/example/androiddevchallenge/timer/mapper/TimerMapper.kt
+++ app/src/main/java/com/example/androiddevchallenge/timer/mapper/TimerMapper.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.androiddevchallenge.mapper
+package com.example.androiddevchallenge.timer.mapper
 
 import com.example.androiddevchallenge.timer.TimeUIModel
 
